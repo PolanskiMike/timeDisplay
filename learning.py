589b191f8b20cde7ff1fdda22d036ab9cff7e2c4
Mike Polanski <micheal.polanski@gmail.com>
2024-12-24 09:59:55 -0500

    learning if conditionals

---
+++ learning.py
@@ -0,0 +1,6 @@
+myName = input("What is your name?\n")
+
+if myName == "Mike":
+    print("Hello" + " " + myName)
+else:
+    print("You're not Mike!")
--- test.py
+++
@@ -1,7 +0,0 @@
-length = 10
-width = 20
-area = length * width
-
-print(length)
-print(width)
-print(area)
