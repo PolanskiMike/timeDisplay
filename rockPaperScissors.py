518ecc348f1f0a65e7f8f43757c282f6475c91eb
Mike Polanski <micheal.polanski@gmail.com>
2024-12-24 11:36:03 -0500

    beginning of loop through if

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -5,21 +5,23 @@ choices.append('spok')
 
 computerChoice = random.choice(choices)
 
-userChoice = input("Do you want rock, paper, or scissors?\n")
+numRounds = input("How many rounds do you want to play?\n")
 
-if computerChoice == userChoice:
-    print('TIE')
-elif userChoice == 'rock' and computerChoice == 'scissors':
-    print('WIN')
-elif userChoice == 'paper' and computerChoice == 'rock':
-    print('WIN')
-elif userChoice == 'scissors' and computerChoice == 'paper':
-    print('WIN')
-elif userChoice == 'lizard' and computerChoice == 'spok':
-    print('WIN')
-elif userChoice == 'spok' and computerChoice == 'lizard':
-    print('WIN')
-else:
-    print('LOSE')
+userChoice = input("Do you want rock, paper, scissors, lizard, spok?\n")
 
-print(computerChoice)
+
+for rounds in range(numRounds):
+    if computerChoice == userChoice:
+        print('TIE')
+    elif userChoice == 'rock' and computerChoice == 'scissors':
+        print('WIN')
+    elif userChoice == 'paper' and computerChoice == 'rock':
+        print('WIN')
+    elif userChoice == 'scissors' and computerChoice == 'paper':
+        print('WIN')
+    elif userChoice == 'lizard' and computerChoice == 'spok':
+        print('WIN')
+    elif userChoice == 'spok' and computerChoice == 'lizard':
+        print('WIN')
+    else:
+        print('LOSE')
